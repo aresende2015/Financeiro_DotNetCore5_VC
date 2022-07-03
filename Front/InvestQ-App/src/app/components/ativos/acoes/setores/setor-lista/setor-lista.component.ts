892d82a3de3c35038cbc42469bcc374cf2f14b2d
aresende2015 <alex.q.resende@gmail.com>
2022-07-03 10:38:17 -0300

    Ajustes

--- Front/InvestQ-App/src/app/components/ativos/acoes/setores/setor-lista/setor-lista.component.ts
+++ Front/InvestQ-App/src/app/components/ativos/acoes/setores/setor-lista/setor-lista.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { SetorService } from './../../../services/setor.service';
+import { SetorService } from '../../../../../services/setor.service';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
